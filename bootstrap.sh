8aaf4cf6ec81a8c347bb17ade05bd0cff4d088d2
hapydevlab <hapydev@hotmail.com>
2024-12-16 01:24:25 +0800

    fix: 修复多条参数无法传入的问题

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-echo 'window.BASE_URL="'${BASE_URL}'"'> /usr/share/nginx/html/envs.js
-echo 'window.CLOUD_PROXY_URL="'${CLOUD_PROXY_URL}'"'> /usr/share/nginx/html/envs.js
+> /usr/share/nginx/html/envs.js
+echo 'window.BASE_URL="'${BASE_URL}'";'>> /usr/share/nginx/html/envs.js
+echo 'window.CLOUD_PROXY_URL="'${CLOUD_PROXY_URL}'";'>> /usr/share/nginx/html/envs.js
+echo 'window.API_HOST_URL="'${API_HOST_URL}'";'>> /usr/share/nginx/html/envs.js
+echo 'window.DOC_HOST_URL="'${DOC_HOST_URL}'";'>> /usr/share/nginx/html/envs.js
 nginx
 tail -f /dev/null
