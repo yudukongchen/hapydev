6a37b2ae36adff04edc1f5452f64f1129a653549
wzz <wangzz5038@foxmail.com>
2025-08-15 17:49:35 +0800

    windows程序构建

---
+++ electron-main.js
@@ -0,0 +1,14 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: { nodeIntegration: true, contextIsolation: false }
+    });
+    win.loadFile(path.join(__dirname, 'dist', 'index.html'));
+}
+
+app.whenReady().then(createWindow);
+app.on('window-all-closed', () => app.quit());
